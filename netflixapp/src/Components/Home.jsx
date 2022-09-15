0b36663ba3a2febbe5262cb89cfb4ac730449ced
Rahul <rahul10kr7@gmail.com>
2022-09-15 10:50:32 +0530

    display card and row complete

--- netflixapp/src/Components/Home.jsx
+++ netflixapp/src/Components/Home.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
-import './Home.scss'
+import React from "react";
+import "./Home.scss";
 
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
 
+const Row = ({ title }) => (
+  <div className="row">
+    <h2>{title}</h2>
+    <div>
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://everyevery.ng/wp-content/uploads/2019/03/InfinityWar5aabd55fed5fa.jpg"
+        }
+      />
+    </div>
+  </div>
+);
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="banner"></div>
+      <Row title={"Popular on Netflix"} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
